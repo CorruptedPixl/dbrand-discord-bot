9722cb3f9d25d94961f672b9e81940b6cee3f2a0
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2020-08-12 00:01:43 +0200

    added command and event handers

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { Client, Collection } = require("discord.js");
-require('dotenv').config()
 const config = require('./config.json');
-// require('handlers/commands')(bot);
-// require('handlers/events')(bot);
-
 const bot = new Client();
+
 bot.commands = new Collection();
 bot.aliases = new Collection();
 bot.prefix = config.prefix;
+
+require('dotenv').config()
+require('./handlers/commands.js')(bot);
+require('./handlers/events.js')(bot);
+
 
 bot.login(process.env.CLIENT_TOKEN);
