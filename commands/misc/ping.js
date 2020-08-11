0ecbcc03a213d935d663bea2c306681ff09151d4
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2020-08-12 00:27:55 +0200

    updated to V12 ws.ping

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -4,8 +4,6 @@ module.exports = {
         description: `pings the bot and reports the... ping...`
     },
     run: async (client, message) => {
-        message.channel.send("Pong...").then(msg => {
-            msg.edit(`<:max:619335115268423691> ${msg.createdTimestamp - message.createdTimestamp} ms`);
-        });
+        message.channel.send(`Pong! <:max:619335115268423691> \nThe current ping is ${Math.floor(client.ws.ping)} ms`);
     }
 }
