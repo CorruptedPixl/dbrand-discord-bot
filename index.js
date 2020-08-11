7c7131e98ae44469b621689f6e1296fef44a06ad
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2020-08-11 20:24:07 +0200

    linked prefix to config file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const { Client, Collection } = require("discord.js");
 require('dotenv').config()
+const config = require('./config.json');
 // require('handlers/commands')(bot);
 // require('handlers/events')(bot);
 
 const bot = new Client();
 bot.commands = new Collection();
 bot.aliases = new Collection();
-bot.prefix = '!';
+bot.prefix = config.prefix;
 
 bot.login(process.env.TOKEN);
