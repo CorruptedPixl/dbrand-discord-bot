bfbcd04abd3f5b365c9aa0250cf89d4e7207ebda
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2020-08-12 16:22:27 +0200

    checks amount of servers it's in

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,16 @@
 module.exports = async (bot) => {
     let status = `over ${bot.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} dipshits`
+    bot.user.setStatus("idle");
     bot.user.setActivity(status, { type: "WATCHING" });
     console.log(`✔ Bot Ready!`);
+
+    switch (bot.guilds.size) {
+        case 1:
+            console.log(`The bot is online in 1 server.`);
+            break;
+
+        default:
+            console.log(`The bot is online in ${bot.guilds.cache.size} servers.`);
+            break;
+    }
 }
