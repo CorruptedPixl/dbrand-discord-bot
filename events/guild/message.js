09c7c3679cd32f93c327766ef0b0943084a71671
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2020-08-12 20:16:14 +0200

    added logging to server channel

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,4 +1,5 @@
 const { prefix } = require("../../config.json");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = async (bot, message) => {
     // Ignore dms or bot messages
@@ -10,4 +11,27 @@ module.exports = async (bot, message) => {
     if (!message.content.startsWith(prefix)) return;
     let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
     if (commandfile) commandfile.run(bot, message, args)
+
+    // command logging
+    console.log(`${message.member.user.tag}${message.member} ran: ${message}`);
+
+    const embed = new MessageEmbed()
+        .setAuthor(message.member.user.tag, message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 64 }))
+        .setDescription(`ran: ${message}`)
+        .setTimestamp()
+        .setFooter(`User ID: ${message.member.id}`, `${message.guild.iconURL()}`);
+
+    switch (true) {
+        case !!commandfile:
+            embed.setColor(0x33de44);
+            break;
+        case !commandfile:
+            embed.setColor(0xff3333);
+            break;
+        default:
+            embed.setColor(0xffbb00);
+            break;
+    }
+
+    bot.channels.cache.get(process.env.logchannelid).send(embed);
 }
