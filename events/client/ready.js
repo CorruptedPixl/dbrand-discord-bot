e9294fea057ef54d81d5167b16b30b8c2a25563c
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2020-08-12 15:29:31 +0200

    status shows member count now

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
-module.exports = async bot => {
-    let status = "With y'alls feelings"
-    bot.user.setActivity(status, { type: "PLAYING" });
+module.exports = async (bot) => {
+    let status = `over ${bot.guilds.cache.reduce((a, g) => a + g.memberCount, 0)} dipshits`
+    bot.user.setActivity(status, { type: "WATCHING" });
 }
