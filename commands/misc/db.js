764b0af3b18d06e4597fc8183dadc720e7d7dc40
CorruptedPixl <38385453+CorruptedPixl@users.noreply.github.com>
2020-12-20 14:37:26 +0100

    Optimized code, at least one request less fetched which brought the caseShipping execution time down from min 820ms to 220ms

--- commands/misc/db.js
+++ commands/misc/db.js
@@ -12,13 +12,14 @@ const { shippingArray, /*howtoArray*/ } = require(`../../commands/command-config
 module.exports = {
   run: async (client, message, args) => {
 
-    //Defines global variables
+    // Defines global variables
     let destination;
-    //Sets standard link to add to later
+    // Sets standard link to add to later
     let link = `https://dbrand.com/`;
 
 
-    if (args.length == 0) {                                                                                     //Deletes user message and sends an error if they don't give any args
+    // Deletes user message and sends an error if they don't give any args
+    if (args.length == 0) {
       message.delete();
       message.channel.send(`No argument. Please use !db *argument*`);
     }
@@ -27,90 +28,39 @@ module.exports = {
     else if (args.length > 0) {
 
       //!Below are the supported functions that handle certain db args
-      const caseGrip = () => {                        //Set link to grip
+      // Set link to grip
+      const caseGrip = () => {
         link = `${link  }shop/grip/#grip-devices`;
         message.delete();
       };
 
-      const caseSupport = () => {                     //Set link to support
+      // Set link to support
+      const caseSupport = () => {
         link = `${link  }contact`;
         message.delete();
       };
 
-      const caseSkin = () => {                        //Set link to skins
+      // Set link to skins
+      const caseSkin = () => {
         link = `${link  }shop/skins`;
         message.delete();
       };
 
-      const casePrism = () => {                       //Set link to Prism
+      // Set link to Prism
+      const casePrism = () => {
         link = `${link  }shop/prism`;
         message.delete();
       };
 
-      //!PROVISIONED FOR REMOVAL IN NEXT RELEASE
       const caseShipping = () => {
         let matches;
-        message.delete();                                                                      //Deletes user's message
-
-        if (args[1] != undefined) {                                                                         //Checks for an argument
-
-          const isTextOnly = /^[A-Z-]+$/i.test(args[1]);                                                    //Tests via a regex if the argument is only characters, not emoji's or slashes or numbers
-          if (!isTextOnly) {
-            message.channel.send(`Country codes and emoji's are not supported. Please type part of the country's name to search.`)
-              .then(msg => {
-                msg.delete({ timeout: msgDeleteDelay });
-              });
-          }
-          const searchTerms = args.slice(1, 3).join(`-`);                                       //combines arg[1] and arg[2] to also find "united-states" and such
-          // console.log(searchTerms);                                                                      //Used for debug
-
-          matches = shippingArray.filter(s => s.includes(searchTerms.toString().toLowerCase()));          //Creates an array with all country's found that match part of the substring (arg1)
-          // console.log(matches);                                                                          //Used for debugging
-
-          if (matches.length > 0) {
-            link = `https://dbrand.com/shipping/${matches[0]}`;
-            destination = matches[0];
-
-            const shippingEmbed = new Discord.MessageEmbed()
-              .setTitle(`Shipping to ${destination}`)
-              .setDescription(`Click the link above to see shipping time to ${destination}. \nYou can also [view all shipping destinations here](https://dbrand.com/shipping)`)
-              .setURL(`${link}`)
-              .setColor(`#ffbb00`)
-              .setTimestamp()
-              .attachFiles([`../dbrand-bot-V2/commands/assets/db-logo.png`])
-              .setFooter(`dbrand.com`, `attachment://db-logo.png`);
-
-            message.channel.send(shippingEmbed);
-          } else if (isTextOnly) {
-            link = `${link  }shipping`;
-
-            const shippingEmbedNotFound = new Discord.MessageEmbed()
-              .setTitle(`Check shipping times here!`)
-              .setDescription(`Country not found.\nYou can [view all shipping destinations here](https://dbrand.com/shipping)`)
-              .setURL(`${link}`)
-              .setColor(`#ffbb00`)
-              .setTimestamp()
-              .attachFiles([`../dbrand-bot-V2/commands/assets/db-logo.png`])
-              .setFooter(`dbrand.com`, `attachment://db-logo.png`);
-            message.channel.send(shippingEmbedNotFound);
-          }
-        } else {
-          message.channel.send(`Please type *(part)* of the county you want to ship to after the command. **!db ship belgium**`)
-            .then(msg => {
-              msg.delete({ timeout: msgDeleteDelay });
-            });
-        }
-      };
-
-      const caseShippingNEW = () => {
-        let matches;
-        //Deletes user's message
+        // Deletes user's message
         message.delete();
 
-        //Checks for an argument
-        if (args[1] != undefined) {
+        // Checks for an argument
+        if (args[1] !== undefined) {
 
-          //Tests via a regex if the argument is only characters, not emoji's or slashes or numbers
+          // Tests via a regex if the argument is only characters, not emoji's or special chars or numbers
           const isTextOnly = /^[A-Z-]+$/i.test(args[1]);
           if (!isTextOnly) {
             message.channel.send(`Emoji's and special chars are not supported. Please type part of the country's name or two letter country code to search.`)
@@ -119,8 +69,7 @@ module.exports = {
               });
           }
 
-          if (args[1].length == 2){
-
+          if (args[1].length === 2 && isTextOnly){
             try {
               // Try getting the data from dbrand's shipping api
               fetch(`${shippingDataUrl}${args[1]}`)
@@ -134,20 +83,20 @@ module.exports = {
                     });
                 });
 
+
               // Create and send the embed with data from the api res
               const fillData = async json => {
                 if (!json.is_valid){
                   throw new Error(`Invalid country code`);
                 }
+
                 const encodedURI = encodeURI(`${link}shipping/${json.country}`);
                 const shippingOptions = await loadShippingOptions(json);
-                // console.error(eval(shippingOptions));
 
                 const dbrandShippingEmbed = new Discord.MessageEmbed()
                   .setTitle(`Shipping to ${json.country}`)
                   .setDescription(`Click the link above to see shipping time to ${json.country}. \nYou can also [view all shipping destinations here](https://dbrand.com/shipping)`)
 				          .setURL(encodedURI)
-                  // .addFields(eval(shippingOptions))
                   .setColor(`#ffbb00`)
                   .setTimestamp()
                   .setThumbnail(`https://dbrand.com/${json.country_flag}`)
@@ -163,11 +112,12 @@ module.exports = {
               const loadShippingOptions = async json => {
                 /* eslint-disable-next-line*/
                 let shippingData = [];
-
+                // Fill in the first response from the initial json we got, then loop over the others to fetch and fill
+                shippingData.push({name: json.shipping_services_available[0].title, value: json[`time-title`]});
+                const shippingServices = json.shipping_services_available.slice(1);
 				
-                for (const shippingOption of json.shipping_services_available) {
-
-                  const pog = await fetch(`${shippingDataUrl}${args[1]}/${shippingOption.url}`)
+                for (const shippingOption of shippingServices) {
+                  await fetch(`${shippingDataUrl}${args[1]}/${shippingOption.url}`)
                     .then(res => res.json())
                     .then(json => shippingData.push({name: shippingOption.title, value: json[`time-title`]}))
                     .catch(err => {
@@ -264,8 +214,8 @@ module.exports = {
       case `p`: casePrism(); break;
 
         //Larger commands
-      case `shipping`: caseShippingNEW(); break;
-      case `ship`: caseShippingNEW(); break;
+      case `shipping`: caseShipping(); break;
+      case `ship`: caseShipping(); break;
 
       default:
         caseUnknown(); break;
